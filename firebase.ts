23ee2b2262132e69aab66c3185cb99fad2fe392f
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2025-07-26 01:42:20 +0530

    feat: setup firebase

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRU3IxRDB7oaqv8i3nconQ0O8pYjjzTp0",
+  authDomain: "cost-calc-23f6b.firebaseapp.com",
+  projectId: "cost-calc-23f6b",
+  storageBucket: "cost-calc-23f6b.firebasestorage.app",
+  messagingSenderId: "111779686913",
+  appId: "1:111779686913:web:f6e9ae299ffac43549ee00"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
